8bfac1a76c7522cb49c0cc64d585b7820ffbc2ec
takada038 <takada038@icloud.com>
2024-11-14 18:25:39 +0000

    適切なコミットメッセージ

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\PostController; 
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/posts', [PostController::class, 'index']);   
